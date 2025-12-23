eb839cb283ddf6af77f3f556f56400d35e9a11b5
Adam Farhan <adamfarhan11@gmail.com>
2025-12-23 15:14:25 -0500

    Initiated prisma basics

---
+++ src/lib/db.ts
@@ -0,0 +1,13 @@
+import { PrismaClient } from "@/generated/prisma/client";
+
+const globalForPrisma = global as unknown as {
+  primsa: PrismaClient;
+};
+
+const prisma = globalForPrisma.primsa || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.primsa = prisma;
+}
+
+export default prisma;
