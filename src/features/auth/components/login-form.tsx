29b3da77d09cad7261357b87a76297d5a3ffba3f
Adam Farhan <adamfarhan11@gmail.com>
2025-12-24 16:17:21 -0500

    Updated theme and auth layout

--- src/features/auth/components/login-form.tsx
+++ src/features/auth/components/login-form.tsx
@@ -82,6 +82,12 @@ export const LoginForm = () => {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/github.svg"
+                      width={20}
+                      height={20}
+                      alt="Github"
+                    />
                     Continue with GitHub
                   </Button>
                   <Button
@@ -90,6 +96,12 @@ export const LoginForm = () => {
                     type="button"
                     disabled={isPending}
                   >
+                    <Image
+                      src="/logos/google.svg"
+                      width={20}
+                      height={20}
+                      alt="Google"
+                    />
                     Continue with Google
                   </Button>
                 </div>
