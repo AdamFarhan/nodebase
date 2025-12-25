33c6dffa2f73611c928e013b6113a82dc6e43a90
Adam Farhan <adamfarhan11@gmail.com>
2025-12-25 13:37:23 -0500

    Added basic AI providers and ran them with inngest

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,19 +1,53 @@
 import prisma from "@/lib/db";
 import { inngest } from "./client";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { createOpenAI } from "@ai-sdk/openai";
+import { createAnthropic } from "@ai-sdk/anthropic";
+import { generateText } from "ai";
 
-export const helloWorld = inngest.createFunction(
-  { id: "hello-world" },
-  { event: "test/hello.world" },
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
+
+export const execute = inngest.createFunction(
+  { id: "execute-ai" },
+  { event: "execute/ai" },
   async ({ event, step }) => {
-    await step.sleep("fetching", "1s");
-    await step.sleep("transcribing", "2s");
-    await step.sleep("processing", "3s");
-    await step.run("create-workflow", () => {
-      return prisma.workflow.create({
-        data: {
-          name: "workflow-from-innest",
-        },
-      });
-    });
+    await step.sleep("pretend", "3s");
+    const { steps: geminiSteps } = await step.ai.wrap(
+      "gemini-generate-text",
+      generateText,
+      {
+        model: google("gemini-2.5-flash"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2+2?",
+      }
+    );
+
+    const { steps: openAiSteps } = await step.ai.wrap(
+      "openai-generate-text",
+      generateText,
+      {
+        model: openai("gpt-5.1"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2+2?",
+      }
+    );
+
+    const { steps: anthropicSteps } = await step.ai.wrap(
+      "anthropic-generate-text",
+      generateText,
+      {
+        model: anthropic("claude-sonnet-4-5"),
+        system: "You are a helpful assistant",
+        prompt: "What is 2+2?",
+      }
+    );
+
+    return {
+      geminiSteps,
+      openAiSteps,
+      anthropicSteps,
+    };
   }
 );
