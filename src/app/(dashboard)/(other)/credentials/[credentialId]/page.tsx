fce0420382d67125d417608481e88ae9c70e5efb
Adam Farhan <adamfarhan11@gmail.com>
2025-12-26 11:55:29 -0500

    Added basic sidebar and app header

---
+++ src/app/(dashboard)/(other)/credentials/[credentialId]/page.tsx
@@ -0,0 +1,12 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+interface PageProps {
+  params: Promise<{
+    credentialId: string;
+  }>;
+}
+export default async function Page({ params }: PageProps) {
+  await requireAuth();
+  const { credentialId } = await params;
+  return <p>Credentials Id: {credentialId} </p>;
+}
